ff89feb943a9c431e1eed3d6b01ff34d52b51a69
Aman <dv451197@gmail.com>
2023-12-25 22:31:44 +0530

    add router

---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import authSlices from "./authSlices";
+
+const store = configureStore({
+  reducer: {
+    auth: authSlices,
+  },
+});
+
+export default store;
