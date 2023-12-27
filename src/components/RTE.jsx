4b14fa0e3d697acc19d853d4b0b4e55d18278753
Aman <dv451197@gmail.com>
2023-12-27 23:09:31 +0530

    added Editor

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,8 +1,59 @@
 import React from "react";
-// import {} from "react-hoo"
+import { Editor } from "@tinymce/tinymce-react";
+import { Controller } from "react-hook-form";
+import { render } from "react-dom";
 
-const RTE = () => {
-  return <div>RTE</div>;
+const RTE = ({ name, control, label, defaultValue = "" }) => {
+  return (
+    <div className="w-full">
+      {label && <label className="inline-block mb-1 pl-1 ">{label}</label>}
+      <Controller
+        name={name || "Blog-content"}
+        control={control}
+        render={
+          { field: { onchange } } >
+          (
+            <Editor
+              initialValue={defaultValue}
+              init={{
+                branding: false,
+                height: 500,
+                menubar: true,
+                plugins: [
+                  "advlist",
+                  "autolink",
+                  "lists",
+                  "link",
+                  "image",
+                  "charmap",
+                  "preview",
+                  "anchor",
+                  "searchreplace",
+                  "visualblocks",
+                  "code",
+                  "fullscreen",
+                  "insertdatetime",
+                  "media",
+                  "table",
+                  "code",
+                  "help",
+                  "wordcount",
+                ],
+                toolbar:
+                  "undo redo | blocks | " +
+                  "bold italic forecolor | alignleft aligncenter " +
+                  "alignright alignjustify | bullist numlist outdent indent | " +
+                  "removeformat | help",
+                content_style:
+                  "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+              }}
+              onEditorChange={onchange}
+            />
+          )
+        }
+      />
+    </div>
+  );
 };
 
 export default RTE;
