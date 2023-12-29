7d55aa0244edc7e63366e77b74384b14ced5daab
Aman <dv451197@gmail.com>
2023-12-29 15:27:33 +0530

    compleate edit post

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import service from "../appwrite/config";
+import Container from "../components/container/Container";
+import PostForm from "../components/post-form/PostForm";
+
+// params => slug => service => getPost => edit
 
 const EditPost = () => {
-  return <div>EditPost</div>;
+  const [post, setPost] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (slug) {
+      service.getPost(slug).then((post) => {
+        if (post) {
+          setPost(post);
+        } else {
+          alert("Coudn't find the post to edit");
+        }
+      });
+    }
+  }, [slug, navigate]);
+  return (
+    <div className="py-6 ">
+      <Container>
+        <PostForm post={post} />
+      </Container>
+    </div>
+  );
 };
 
 export default EditPost;
