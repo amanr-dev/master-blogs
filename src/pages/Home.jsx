cf034a488b5d6e1d0933bf89925164946fbcc09a
Aman <dv451197@gmail.com>
2023-12-31 22:39:49 +0530

    error in Signup Compon...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,46 +4,42 @@ import PostCard from "../components/PostCard";
 import Container from "../components/container/Container";
 import { Link } from "react-router-dom";
 import Button from "../components/Button";
-import Header from "../components/header/Header";
 
 const Home = () => {
   const [posts, setPosts] = useState(null);
 
-  // useEffect(() => {
-  //   service.getPosts([]).then((post) => {
-  //     if (post) {
-  //       setPosts(post.documents);
-  //     }
-  //   });
-  // }, []);
+  useEffect(() => {
+    service.getPosts([]).then((post) => {
+      if (post) {
+        setPosts(post.documents);
+      }
+    });
+  }, []);
 
-  // if (posts.length === 0) {
-  //   return (
-  //     <div className="w-full py-8">
-  //       <Container>
-  //         <div className="flex flex-wrap">
-  //           <h4>Please Login to Read Posts</h4>
-  //           <Link to="/login">
-  //             <Button bgColor="bg-blue-500" textColor="text-white">
-  //               Login
-  //             </Button>
-  //           </Link>
-  //         </div>
-  //       </Container>
-  //     </div>
-  //   );
-  // }
+  if (!posts?.length) {
+    return (
+      <div className="w-full py-8">
+        <div className="flex flex-wrap justify-center items-center flex-col ">
+          <h4>Please Login to Read Posts</h4>
+          <Link to="/login">
+            <Button bgColor="bg-blue-500" textColor="text-white">
+              Login
+            </Button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="w-full py-8">
       <Container>
-        {/* <Header /> */}
         <div className="flex flex-wrap">
-          {/* {posts.map((post) => (
+          {posts.map((post) => (
             <div className="py-2 flex-wrap">
               <PostCard {...post} key={post.id} />
             </div>
-          ))} */}
+          ))}
           Hello to home
         </div>
       </Container>
